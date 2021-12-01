3f64b42aa92670b61ac85647a7b9251cb90be160
Guboneui <starku2249@naver.com>
2021-12-01 22:21:15 +0900

    Initial Commit

---
+++ iOS Small Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Small Project
+//
+//  Created by 구본의 on 2021/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
